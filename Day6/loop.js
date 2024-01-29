2ebc0b57d64ebe51cc667c55e60dfade0eb6a682
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-29 18:09:29 +0300

    Done

--- Day6/loop.js
+++ Day6/loop.js
@@ -4,3 +4,9 @@ console.log(E)
 let shayo =["Mobile","Jiimo","Disti"]
 
 console.log(shayo)
+
+
+let khudaar =["Xabuub","tufaax","cambe","liinmacan"]
+for(kh=3;kh<khudaar.length; kh++){
+        console.log (khudaar[kh])
+}
