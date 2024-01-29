6294702908447ad319155f7bf2e8bb8da1358e62
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-29 18:29:08 +0300

    Done

--- Day6/function.js
+++ Day6/function.js
@@ -15,3 +15,14 @@ function add(a,b){
         return a+b;
 }
 console.log(3+4)
+
+function multiply(a,b){
+        return a*b ;
+}
+console.log(5*4)
+
+
+function salaam(name){
+        return"Hello World" + "" +name;
+}
+console.log(salaam("Aisha"));
