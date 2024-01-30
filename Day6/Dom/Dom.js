f2804fe43dd67149104f255e20b4ad7786faa0b3
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 17:53:11 +0300

    Done

--- Day6/Dom/Dom.js
+++ Day6/Dom/Dom.js
@@ -1,2 +1,5 @@
 document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah ee DOM";
 document.querySelector('div').innerText = "31/1/2024";
+
+
+document.getElementById('h5').innerText = "Miss Hamda"
