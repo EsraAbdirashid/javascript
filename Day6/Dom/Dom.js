e88d8c7b274872072d9e78b4cee5799b99e89653
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-30 17:55:29 +0300

    Done

--- Day6/Dom/Dom.js
+++ Day6/Dom/Dom.js
@@ -2,4 +2,5 @@ document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah e
 document.querySelector('div').innerText = "31/1/2024";
 
 
-document.getElementById('h5').innerText = "Miss Hamda"
+document.getElementById('h5').innerText = "Miss Hamda";
+document.getElementById('h6').innerText = "DAD";
