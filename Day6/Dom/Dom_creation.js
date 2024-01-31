bd787de4a9c16911f3f2fd6173faa507256a8232
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-31 17:31:38 +0300

    Done

--- Day6/Dom/Dom_creation.js
+++ Day6/Dom/Dom_creation.js
@@ -4,3 +4,6 @@ let personName = document.createElement('name')
 personName.innerText="Esra Abdirashid";
 
 body.appendChild(personName);
+
+personName.style.backgroundColor='white';
+personName.style.color='orange'
