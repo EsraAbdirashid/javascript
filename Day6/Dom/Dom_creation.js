0b72c91ca23de295f4806ca35f4acd8a03ed495f
EsraAbdirashid <israApdirashid@gmail.com>
2024-01-31 17:39:54 +0300

    Done

--- Day6/Dom/Dom_creation.js
+++ Day6/Dom/Dom_creation.js
@@ -7,6 +7,6 @@ body.appendChild(personName);
 
 personName.style.backgroundColor='white';
 personName.style.color='orange';
-
-personName.style.marginLeft ='450px'
-personName.style.fontSize='40px'
+personName.style.marginLeft ='450px';
+personName.style.fontSize='40px';
+personName.style.padding='20px';
