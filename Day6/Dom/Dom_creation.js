b8e10f74d92968c184f757ac4465b10f4f490441
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-01 18:14:55 +0300

    done

--- Day6/Dom/Dom_creation.js
+++ Day6/Dom/Dom_creation.js
@@ -1,27 +1,17 @@
 let body = document.querySelector('body');
 let personName = document.createElement('name');
-// let image = document.createElement('img');
-let history = document.createElement('His')
+
 
 personName.innerText="Esra Abdirashid";
-// img.src = './img.png';
-History.innerText ="I'am fullstack Dev (website design, programming ,developer)"
+
+
 
 body.appendChild(personName);
-// body.appendChild(img);
-body.appendChild(His);                                          
 
-personName.style.backgroundColor='white';
-personName.style.color='orange';
-personName.style.marginLeft ='450px';
-personName.style.fontSize='40px';
-personName.style.padding='20px';
-personName.style.borderRadius='10px';
-personName.style.marginTop='10px';
+let mypersonName = `
+color:orange;
+backgroun-color: white;
+text-align:center;
+font-size : 40px;
 
-// styling history by using backtill symbol
-
-// History.style.His=`
-// backgroundColor= white;
-// `
-// History.styleText.myHis
+`
