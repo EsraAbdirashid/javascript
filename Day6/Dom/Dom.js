c03a83a5cf826e121c87643181dfd086b2f69dbb
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-01 18:31:59 +0300

    Done

--- Day6/Dom/Dom.js
+++ Day6/Dom/Dom.js
@@ -1,18 +1,18 @@
 
 //  bytagName
-// document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah ee DOM";
-// document.querySelector('div').innerText = "31/1/2024";
+document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah ee DOM";
+document.querySelector('div').innerText = "31/1/2024";
 
 // // byId
-// document.getElementById('h5').innerText = "Miss Hamda";
-// document.getElementById('h6').innerText = "DAD";
-// // by className
-// document.querySelector('.p').innerText = "hhhhhhhhh";
-// document.querySelector('.M').innerHTML = "Xisaab";
+document.getElementById('h5').innerText = "Miss Hamda";
+document.getElementById('h6').innerText = "DAD";
+// by className
+document.querySelector('.p').innerText = "hhhhhhhhh";
+document.querySelector('.M').innerHTML = "Xisaab";
 
-// // variable lagu shubayo
-// let sheeko = document.querySelector('.sheeko')
-// sheeko.innerText = "waxaa mahad oodhan iska leh Allah subxanahu watacalaa🤲";
+// variable lagu shubayo
+let sheeko = document.querySelector('.sheeko')
+sheeko.innerText = "waxaa mahad oodhan iska leh Allah subxanahu watacalaa🤲";
 
 // let salaan = document.querySelector('#salaan')
 // salaan.innerHTML = "ASC WR WB";
