eeb0775691ad250531eb647537683b0f65daac55
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-01 18:30:43 +0300

    Done

--- Day6/Dom/Dom.js
+++ Day6/Dom/Dom.js
@@ -1,6 +1,6 @@
 
 //  bytagName
-document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah ee DOM";
+// document.querySelector('h2').innerText = "kusoo dhawow cashirka muraajicada ah ee DOM";
 document.querySelector('div').innerText = "31/1/2024";
 
 // // byId
