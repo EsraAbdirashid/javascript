07925bc884d69d667abae82cb2267f5b227a2a5f
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-01 18:21:21 +0300

    Done

--- Day6/Dom/Dom_creation.js
+++ Day6/Dom/Dom_creation.js
@@ -1,7 +1,7 @@
 let body  = document.querySelector('body');
 let message = document.createElement('p')
 let title = document.createElement('span');
-let image = document.createElement('img');
+let image = document.createElement('img')
 let btnClick  = document.querySelector('button')
 
 // message.textContent = 'Mss Esra!';
