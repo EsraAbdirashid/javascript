b547c5ec0e3232d01face7e710025af284a345e4
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-02 17:11:19 +0300

    Done

---
+++ onsubmit.js
@@ -0,0 +1,9 @@
+let form = document.querySelector('form');
+// let input = document.querySelector('#input-value');
+
+
+// form.addEventListener('click',(e)=>){
+//         e.preventDefault();
+//         input.value;
+//         console.log(input.value);
+// }
