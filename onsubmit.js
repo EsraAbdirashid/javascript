bc7b767c6f261cf2cfb896f783b35617d5a08f0b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-02 17:36:29 +0300

    Done

--- onsubmit.js
+++ onsubmit.js
@@ -7,7 +7,7 @@ form.addEventListener('click',(e)=>{
         e.preventDefault();
         input.value;
         let h2 = document.createElement('h2');
-
+         h2.innerHTML=input.value;
         div.appendChild(h2)
    
 })
