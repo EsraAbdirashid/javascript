ea74a5a2fe7ee65db41325cd20e951f953bab68f
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-05 18:03:38 +0300

    Done

--- localStorage.js
+++ localStorage.js
@@ -2,3 +2,7 @@ const inputTask = document.getElementById("InputTask");
 const btnAdd = document.getElementById("btnAdd");
 const div = document.querySelector("div");
 const btnDelete = document.querySelector("btnDelete");
+
+btnAdd.addEventListener("click", ()=>{
+        const data = inputTask.value ;
+})
