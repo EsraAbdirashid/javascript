3f66ea8fb1edc73e5d38c0c3a3e5fc1a4696357e
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:52:59 +0300

    Done

--- spreadOprators.js
+++ spreadOprators.js
@@ -3,14 +3,14 @@
 // let Person = [
 //     {id:1,name: "Hasi",address: "123Street"}
 // ]
-// let newPerson =[
-//     { id:2,name: "Moumin",address: "456Street"},
-//     {id:3,name: "JigJiga",address: "789Street"}   
-// ]
-// const getPerson = ()=>{
-//     return [...Person,newPerson]
-// }
-// let getData = getPerson();
+let newPerson =[
+    { id:2,name: "Moumin",address: "456Street"},
+    {id:3,name: "JigJiga",address: "789Street"}   
+]
+const getPerson = ()=>{
+    return [...Person,newPerson]
+}
+let getData = getPerson();
 console.log(getData);
 
 // let result = [...Items, newItems];
