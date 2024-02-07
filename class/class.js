20248f4682bf9b96122a1ff97113f36b944ab614
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:33:38 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -15,17 +15,3 @@ class Customers{
                 `
         }
 }
-const person1 =new Customers (1,'Esra','Abdirashid',+7865432,new Date);
-const person2 =new Customers (2,'Esra','Abdirashid',+7865432,new Date);
-const person3 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
-const person4 =new Customers (4,'Esra','Abdirashid',+7865432,new Date);
-const person5 =new Customers (5,'Esra','Abdirashid',+7865432,new Date);
-const person6 =new Customers (6,'Esra','Abdirashid',+7865432,new Date);
-const person7 =new Customers (7,'Esra','Abdirashid',+7865432,new Date);
-const person8 =new Customers (8,'Esra','Abdirashid',+7865432,new Date);
-const person9 =new Customers (9,'Esra','Abdirashid',+7865432,new Date);
-const person10 =new Customers (10,'Esra','Abdirashid',+7865432,new Date);
-const person11 =new Customers (11,'Esra','Abdirashid',+7865432,new Date);
-const person12 =new Customers (12,'Esra','Abdirashid',+7865432,new Date);
-const person13 =new Customers (13,'Esra','Abdirashid',+7865432,new Date);
-console.log(person1,person2,person3,person4,person5,person6,person7,person8,person9,person10,person11,person12,person13)
