d4c6014e1dbc69e9bd1981e6941699e2a0e6a621
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:26:53 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -16,6 +16,15 @@ class Customers{
         }
 }
 const person1 =new Customers (1,'Esra','Abdirashid',+7865432,new Date);
-const person2 =new Customers (2,'Esra','Abdirashid',+7865432,new Date)
-const person3 =new Customers (3,'Esra','Abdirashid',+7865432,new Date)
+const person2 =new Customers (2,'Esra','Abdirashid',+7865432,new Date);
+const person4 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person5 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person6 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person7 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person8 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person9 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person9 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person9 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person9 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
+const person9 =new Customers (3,'Esra','Abdirashid',+7865432,new Date);
 console.log(person1,person2,person3)
