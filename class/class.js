a4fb8f23bc755956abf7c1934fd21ac0ffa2b256
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:25:55 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -16,4 +16,6 @@ class Customers{
         }
 }
 const person1 =new Customers (1,'Esra','Abdirashid',+7865432,new Date)
-console.log(person1)
+const person2 =new Customers (2,'Esra','Abdirashid',+7865432,new Date)
+const person3 =new Customers (3,'Esra','Abdirashid',+7865432,new Date)
+console.log(person1,person2,person3)
