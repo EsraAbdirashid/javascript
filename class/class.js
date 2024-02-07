5e8aeb7ab1f4ac3587075f202f489202f0db7663
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:31:20 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -28,4 +28,4 @@ const person10 =new Customers (10,'Esra','Abdirashid',+7865432,new Date);
 const person11 =new Customers (11,'Esra','Abdirashid',+7865432,new Date);
 const person12 =new Customers (12,'Esra','Abdirashid',+7865432,new Date);
 const person13 =new Customers (13,'Esra','Abdirashid',+7865432,new Date);
-console.log(person1,person2,person3,person4,person5,person6,person7,person8,person9,person10,person11,)
+console.log(person1,person2,person3,person4,person5,person6,person7,person8,person9,person10,person11,person12,)
