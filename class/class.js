c58e8601cf0421bb4aaa1f356fa560d88eb17cc9
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:11:54 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -2,5 +2,6 @@ class Customers{
         constructor(id, firstName, lastName, phone, dateTime){
                 this.id = id;
                 this.firstName =firstName;
+                this.lastName= lastName;
         }
 }
