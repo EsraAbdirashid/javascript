72047a80d203629e66887fbad40b814ea83dd0f3
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:53:10 +0300

    Done

--- spreadOprators.js
+++ spreadOprators.js
@@ -1,8 +1,8 @@
-// let Items = ["javascript","TypeScript","React"];
-// let newItems = ["NodeJs","PostgreSQL","TailwindCss"]
-// let Person = [
-//     {id:1,name: "Hasi",address: "123Street"}
-// ]
+let Items = ["javascript","TypeScript","React"];
+let newItems = ["NodeJs","PostgreSQL","TailwindCss"]
+let Person = [
+    {id:1,name: "Hasi",address: "123Street"}
+]
 let newPerson =[
     { id:2,name: "Moumin",address: "456Street"},
     {id:3,name: "JigJiga",address: "789Street"}   
