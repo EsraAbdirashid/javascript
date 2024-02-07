dfe48797e3fc8f280cbc935295af1f932ea7c5e3
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-07 17:26:09 +0300

    Done

--- class/class.js
+++ class/class.js
@@ -15,7 +15,7 @@ class Customers{
                 `
         }
 }
-const person1 =new Customers (1,'Esra','Abdirashid',+7865432,new Date)
+const person1 =new Customers (1,'Esra','Abdirashid',+7865432,new Date);
 const person2 =new Customers (2,'Esra','Abdirashid',+7865432,new Date)
 const person3 =new Customers (3,'Esra','Abdirashid',+7865432,new Date)
 console.log(person1,person2,person3)
