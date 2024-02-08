b9bb1f4c58c01b62507ce8d9f99911ea8c062f35
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-08 17:10:53 +0300

    Done

--- Es6festure.js
+++ Es6festure.js
@@ -3,5 +3,5 @@
 const numbers = [7,17,27,37,47,57,];
 const filterNumbers = numbers.filter(Number=>{
         const result =Number <=37? "yes" :"No";
-        console.log(result)
+        console.log(result);
 })
