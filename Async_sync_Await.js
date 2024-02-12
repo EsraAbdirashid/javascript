789074f4c56db1f6186e8d85b0aed0c25770e089
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-12 19:17:07 +0300

    Done

--- Async_sync_Await.js
+++ Async_sync_Await.js
@@ -1,15 +1,15 @@
 const p = document.querySelector("p");
 const span = document.querySelector("span");
 const pre = document.querySelector("pre");
-const div = document.querySelector('div')
-const getData = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((user) => {
-        const markup = `<p>userId:${user.id}</p>  <pre> Title: ${user.title}</pre> <span>Body:${user.body} </span> `;
-        div.insertAdjacentHTML("beforeend", markup);
-      });
-    });
-};
-getData();
+// const div = document.querySelector('div')
+// const getData = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       data.forEach((user) => {
+//         const markup = `<p>userId:${user.id}</p>  <pre> Title: ${user.title}</pre> <span>Body:${user.body} </span> `;
+//         div.insertAdjacentHTML("beforeend", markup);
+//       });
+//     });
+// };
+// getData();
