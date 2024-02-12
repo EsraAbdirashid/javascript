fc1998aec83c9a0d1a7fc1c0a88d86f9be1a9f22
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-12 19:31:19 +0300

    Done

--- Async_sync_Await.js
+++ Async_sync_Await.js
@@ -5,8 +5,8 @@ const div = document.querySelector('div')
 const getData = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
-//     .then((data) => {
-//       data.forEach((user) => {
+    .then((data) => {
+      data.forEach((user) => {
 //         const markup = `<p>userId:${user.id}</p>  <pre> Title: ${user.title}</pre> <span>Body:${user.body} </span> `;
 //         div.insertAdjacentHTML("beforeend", markup);
 //       });
